b3d31e16542343647880ff7a5168d56e6a1b1a4a
CodeXSavage6s <iamsavage113377@gmail.com>
2026-06-27 00:10:04 +0100

    auth UI

---
+++ components/form/InputField.tsx
@@ -0,0 +1,36 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FieldValues } from "react-hook-form";
+//import { FormInputPops } from "@/types/form";
+
+const InputField = <T extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  register,
+  validation,
+  type = "text",
+  disabled,
+  value,
+  error,
+}: FormInputPops<T>) => {
+  return (
+    <div>
+      <Label htmlFor={name} className="mb-2">{label}</Label>
+      <Input
+        id={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        {...register(name, validation)}
+        className="p-2 h-[45px]"
+      />
+      {error && (
+        <p className="text-sm text-red-500 mt-1">{error.message}</p>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
